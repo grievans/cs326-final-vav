3509319352290aff33c0c4459856dcc6dc1a5db2
JoJo-19 <89957410+JoJo-19@users.noreply.github.com>
2021-11-05 23:37:38 -0400

    just testing out with the server

--- server.js
+++ server.js
@@ -3,6 +3,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    // console.log(req.headers);
+    // console.log(req.url);
+    // console.log(req.ip);
+    // console.log(req.method);
+    // console.log(req.protocol);
+    // console.log(req.path);
+    // console.log(req.query);
+    // console.log(req.subdomains);
+    // console.log(req.params);
+    res.status(404).end();
+});
+
+// app.post("/");
+
 app.listen(3000, err => {
     if (err) {
         console.log("problem!!", err);
