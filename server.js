fc3335ab9547522ac503d9dfdc50f7cb3330d2ad
grievans <gevans@umass.edu>
2021-11-06 22:44:17 -0400

    added missing argument

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ app.get("*", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, (err) => {
     if (err) {
         console.log("problem!!", err);
         return;
