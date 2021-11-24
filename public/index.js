c50bc3d92bf088dd148f4d855bc84b9d45dc0819
grievans <gevans@umass.edu>
2021-11-24 16:17:43 -0500

    Again

--- public/index.js
+++ public/index.js
@@ -1,34 +1,34 @@
 function setup() {
-    document.getElementById("loginButton").addEventListener("click", login);
+    // document.getElementById("loginButton").addEventListener("click", login);
     document.getElementById("createAccountButton").addEventListener("click", createAccount);
 }
-// async function login(event) {
-//     event.preventDefault();
-//     // console.log("B")
-//     const email = document.getElementById("userEmail").value;
-//     const password = document.getElementById("userPassword").value;
-//     const data = {"user_email":email, "password":password};
-//     fetch("/user/login",{
-//         method: "POST",
-//         cache: "no-cache",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(data),
-//         redirect: 'follow'
-//     }).then((response) => {
-//         return response.json();
-//     }).then((data) => {
-//         if (data["login_status"] === "valid") {
-//             // window.localStorage.setItem('session', data["session_token"]);
-//             // window.localStorage.setItem('user_email', email);
-//             window.location.href = "./welcome.html";
-//         } else {
-//             alert("Invalid login credentials.");
-//         }
-//     });
-//     // console.log("A")
-// }
+async function login(event) {
+    event.preventDefault();
+    // console.log("B")
+    const email = document.getElementById("userEmail").value;
+    const password = document.getElementById("userPassword").value;
+    const data = {"user_email":email, "password":password};
+    fetch("/user/login",{
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+        redirect: 'follow'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        if (data["login_status"] === "valid") {
+            // window.localStorage.setItem('session', data["session_token"]);
+            // window.localStorage.setItem('user_email', email);
+            window.location.href = "./welcome.html";
+        } else {
+            alert("Invalid login credentials.");
+        }
+    });
+    // console.log("A")
+}
 async function createAccount(event) {
     event.preventDefault();
     const email = document.getElementById("userEmail").value;
